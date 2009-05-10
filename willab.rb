eabd10eb4ed075ac42a0ace800e69200e8af3eb8
Juhamatti Niemelä <iiska@iki.fi>
2009-05-10 09:07:56 +0300

    Cache xml file locally and use it if it's no older than 60 seconds.

--- willab.rb
+++ willab.rb
@@ -1,18 +1,34 @@
 #! /usr/bin/env ruby
+# -*- coding: utf-8 -*-
 
 require 'net/http'
 require 'uri'
 require 'rexml/document'
 include REXML
 
-url = URI.parse('http://weather.willab.fi/weather.xml')
-http = Net::HTTP.new(url.host, url.port)
-req = Net::HTTP::Get.new(url.path)
-doc = Document.new(http.request(req).body)
+tmpfile = "/tmp/willab.xml"
 
-weather = doc.root.elements[1];
+# Don't load server too much and use tmpfile if it's fresh enough
+if File.exists?(tmpfile) && ( (Time.now - File.mtime(tmpfile)) < 60 )
+  @doc = Document.new(File.new(tmpfile, "r").read)
+else
+  url = URI.parse('http://weather.willab.fi/weather.xml')
+  http = Net::HTTP.new(url.host, url.port)
+  req = Net::HTTP::Get.new(url.path)
+  @doc = Document.new(http.request(req).body)
+  File.new(tmpfile, "w").write(@doc)
+end
+
+weather = @doc.root.elements[1];
 
 ARGV.each do |i|
-  puts weather.elements[i].text + "˚" +
-    weather.elements[i].attribute("unit").value
+  u = weather.elements[i].attribute("unit").value
+  if (u=="degrees")
+    u = "˚"
+  elsif (u=="C")
+    u = "˚" + u
+  elsif (u!="%")
+    u = " " + u
+  end
+  puts weather.elements[i].text + u
 end
